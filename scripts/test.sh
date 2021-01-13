1f828bcd4dd99bf7427bd00f951bec611d5c3e1d
jiangwenj02 <jiangwenj02@gmail.com>
2021-01-13 17:48:21 +0800

    fix scripts

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-CUDA_VISIBLE_DEVICES=8,9,10,11 ./tools/dist_test.sh configs/polyp/solov2_r50_fpn_polyp_8gpu_3x.py work_dirs/solov2_r50_fpn_8gpu_3x_poly/latest.pth 4 --eval segm --json_out work_dirs/solov2_r50_fpn_8gpu_3x_poly/result.json
+CUDA_VISIBLE_DEVICES=8 python tools/test.py configs/polyp/solov2_r50_fpn_polyp_8gpu_3x.py work_dirs/solov2_r50_fpn_8gpu_3x_poly/latest.pth --eval segm --json_out work_dirs/solov2_r50_fpn_8gpu_3x_poly/result.json
+#CUDA_VISIBLE_DEVICES=8,9,10,11 ./tools/dist_test.sh configs/polyp/solov2_r50_fpn_polyp_8gpu_3x.py work_dirs/solov2_r50_fpn_8gpu_3x_poly/latest.pth 4 --eval segm --json_out work_dirs/solov2_r50_fpn_8gpu_3x_poly/result.json
