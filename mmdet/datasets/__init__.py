1670e1dac287e3bbf0ed6ff3a7caa402260afcc5
jiangwenj02 <jiangwenj02@gmail.com>
2021-02-25 20:38:40 +0800

    adding dataset

--- mmdet/datasets/__init__.py
+++ mmdet/datasets/__init__.py
@@ -10,10 +10,10 @@ from .wider_face import WIDERFaceDataset
 from .xml_style import XMLDataset
 from .polyp import PolypDataset
 from .gang import GangDataset
-
+from .adenomatous import Adenomatous
 __all__ = [
     'CustomDataset', 'XMLDataset', 'CocoDataset', 'VOCDataset', 'PolypDataset',
     'CityscapesDataset', 'GroupSampler', 'DistributedGroupSampler',
     'build_dataloader', 'ConcatDataset', 'RepeatDataset', 'WIDERFaceDataset',
-    'DATASETS', 'build_dataset', 'GangDataset'
+    'DATASETS', 'build_dataset', 'GangDataset', 'Adenomatous'
 ]
