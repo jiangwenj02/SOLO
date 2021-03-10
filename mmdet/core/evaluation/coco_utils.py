19a57d4d1a9815376f548ab130067152bb476a52
jiangwenj02 <jiangwenj02@gmail.com>
2021-03-10 12:12:42 +0800

    fix some bugs

--- mmdet/core/evaluation/coco_utils.py
+++ mmdet/core/evaluation/coco_utils.py
@@ -221,6 +221,8 @@ def segmbbox2json_segmbbox(dataset, results):
     bbox_json_results = []
     for idx in range(len(dataset)):
         img_id = dataset.img_ids[idx]
+        if idx > 5:
+            break
         seg = results[idx][1]
         bbox = results[idx][0]
         for label in range(len(bbox)):
